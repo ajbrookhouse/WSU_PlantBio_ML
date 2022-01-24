6a8ef1ae4b6e046559fc5cb6c01146bca34de522
ajbrookhouse <halfmoonprogramming@gmail.com>
2022-01-24 15:25:29 -0500

    update

--- gui.py
+++ gui.py
@@ -6,6 +6,10 @@ aqua is ok
 
 """
 
+#######################################
+# Imports                             #
+####################################### 
+
 import tkinter as tk
 import tkinter.ttk as ttk
 # from ttkbootstrap import Style as bootStyle
@@ -38,6 +42,10 @@ import traceback
 import sys
 import argparse
 import numpy as np
+
+#######################################
+# Array Manipulations Function        #
+####################################### 
 
 def getImagesForLabels(d, index):
 	indexesToCheck = []
@@ -102,6 +110,10 @@ def arrayToMesh(d, index):
 	print('Calculating final Normals')
 	mesh.compute_vertex_normals()
 	return mesh
+
+#######################################
+# Thread Workers                      #
+####################################### 
 
 def OutputToolsMakeGeometriesThreadWorker(h5path, makeMeshs, makePoints, streamToUse):
 	with redirect_stdout(streamToUse):
@@ -219,6 +231,10 @@ def getImageForLabelNaming(images, labelArray, index, filename):
 	plt.imshow(images)
 	plt.savefig(filename)
 
+#######################################
+# Remote Server Functions             #
+####################################### 
+
 def getRemoteFile(url, uname, passw, filenameToGet, filenameToStore):
 	client = paramiko.SSHClient()
 	client.load_system_host_keys()
@@ -256,9 +272,9 @@ def getSubmissionScriptAsString(template, memory, time, config, outputDirectory)
 	stringTemplate.replace('{outputFileDir}',outputDirectory)
 	return stringTemplate
 
-def MessageBox(message, title=None):
-	print(message)
-	tk.messagebox.showinfo(title=title, message=message)
+#######################################
+# Machine Learning Helper Functions   #
+####################################### 
 
 def get_args():
 	parser = argparse.ArgumentParser(description="Model Training & Inference")
@@ -417,15 +433,6 @@ def redirect_argv(*args):
 	yield
 	sys.argv = sys._argv
 
-# def trainThreadWorker(cfg, stream, button):
-# 	try:
-# 		with redirect_stdout(stream):
-# 			with redirect_stderr(stream):
-# 				trainFromMain(cfg)
-# 	except:
-# 		traceback.print_exc()
-# 	button['state'] = 'normal'
-
 def trainThreadWorker(cfg, stream):
 	with redirect_stdout(stream):
 		try:
@@ -473,14 +480,24 @@ def VisualizeThreadWorker(fileToVisualize, streamToUse, voxel_size=None):
 			print('Critical Error:')
 			traceback.print_exc()
 
-# class TextboxStream(StringIO): # Replaced by MemoryStream, works a lot nicer with the threads. This was causing issues.
-# 	def __init__(self, widget, maxLen = None):
-# 		super().__init__()
-# 		self.widget = widget
+#######################################
+# TK Helper Functions                 #
+####################################### 
 
-# 	def write(self, string):
-# 		self.widget.insert("end", string)
-# 		self.widget.see('end')
+class TextboxStream(StringIO): # Replaced by MemoryStream, works a lot nicer with the threads. This was causing issues.
+	def __init__(self, widget, maxLen = None):
+		super().__init__()
+		self.widget = widget
+
+	def write(self, string):
+		self.widget.insert("end", string)
+		self.widget.see('end')
+
+
+def MessageBox(message, title=None):
+	print(message)
+	tk.messagebox.showinfo(title=title, message=message)
+
 
 class MemoryStream(StringIO):
 	def __init__(self):
@@ -489,6 +506,180 @@ class MemoryStream(StringIO):
 
 	def write(self, string):
 		self.text = self.text + string
+
+
+class FileChooser(ttk.Frame):
+    def __init__(self, master=None, labelText='File: ', changeCallback=False, **kw):
+
+        self.changeCallback = changeCallback
+        ttk.Frame.__init__(self, master, **kw)
+        self.label = ttk.Label(self)
+        self.label.configure(text=labelText)
+        self.label.grid(column='0', row='0')
+
+        self.sv = StringVar()
+        self.sv.trace_add("write", self.entryChangeCallback)
+        self.entry = ttk.Entry(self, textvariable=self.sv)
+        self.entry.grid(column='1', row='0')
+        self.sv.set('')
+
+        self.button = ttk.Button(self)
+        self.button.configure(cursor='arrow', text='Choose File')
+        self.button.grid(column='3', row='0')
+        self.button.configure(command=self.ChooseFileButtonPress)
+
+        self.filepath = self.entry.get()
+
+    def entryChangeCallback(self, sv, three, four):
+        self.filepath = self.getFilepath()
+        if self.changeCallback != False:
+            self.changeCallback()
+
+
+    def ChooseFileButtonPress(self):
+        self.filepath = plyer.filechooser.open_file()
+        self.sv.set(self.filepath)
+
+    def getFilepath(self):
+        return self.entry.get()
+
+
+class LayerNamer(ttk.Frame):
+    def __init__(self, master=None, nameText='', **kw):
+        ttk.Frame.__init__(self, master, **kw)
+        self.label = ttk.Label(self)
+        self.label.configure(text='Name of Layer: ')
+        self.label.grid(column='0', row='0')
+
+        self.sv = StringVar()
+        self.sv.trace_add("write", self.entryChangeCallback)
+        self.entry = ttk.Entry(self, textvariable=self.sv)
+        self.entry.grid(column='1', row='0')
+
+        self.button = ttk.Button(self)
+        self.button.configure(cursor='arrow', text='View')
+        self.button.grid(column='2', row='0')
+        self.button.configure(command=self.ViewButtonPress)
+
+        self.nameText = nameText
+        print(nameText)
+
+    def entryChangeCallback(self, sv, three, four):
+        # Don't know what sv, three, and four are for, however they are needed
+        self.nameText = self.entry.get()
+        self.master.update()
+
+    def ViewButtonPress(self):
+        pass
+
+
+class LayerNamersContainer(ttk.Frame):
+    def __init__(self, master=None, **kw):
+        ttk.Frame.__init__(self, master, **kw)
+        self.LayerNamers = []
+
+        self.frameToExpand = LayerNamer(self)
+        self.frameToExpand.configure(height='200', width='200')
+        self.frameToExpand.grid(side='top')
+
+        LayerNamer1 = LayerNamer(self)
+        self.frameToExpand.configure(height='200', width='200')
+        self.frameToExpand.pack(side='top')
+
+        self.label = ttk.Label(self)
+        self.label.configure(text='Name of Layer: ')
+        self.label.grid(column='0', row='0')
+
+        self.sv = StringVar()
+        self.sv.trace_add("write", self.entryChangeCallback)
+        self.entry = ttk.Entry(self, textvariable=self.sv)
+        self.entry.grid(column='1', row='0')
+
+        self.button = ttk.Button(self)
+        self.button.configure(cursor='arrow', text='View')
+        self.button.grid(column='2', row='0')
+        self.button.configure(command=self.ViewButtonPress)
+
+        self.nameText = nameText
+
+    def entryChangeCallback(self, sv, three, four):
+        # Don't know what sv, three, and four are for, however they are needed
+        self.nameText = self.entry.get()
+        print(self.nameText)
+
+    def ViewButtonPress(self):
+        pass
+
+
+class LayerVisualizerRow(ttk.Frame):
+    def __init__(self, master, color, index, changeCallback=False, **kw):
+        ttk.Frame.__init__(self, master, **kw)
+
+        self.fileChooser = FileChooser(self, changeCallback = changeCallback)
+        self.fileChooser.grid(column='0', row='0')
+
+        self.colorButton = ttk.Button(self)
+        self.colorButton.configure(cursor='arrow', text='Choose Color')
+        self.colorButton.grid(column='2', row='0')
+        self.colorButton.configure(command=self.ChooseColor)
+
+        self.master = master
+        self.index = index
+
+    def ChooseColor(self):
+        self.color = colorchooser.askcolor(title ="Choose Color For Layer " + str(self.index))
+        print(self.color)
+
+    def GetColor(self):
+        return self.color
+
+    def GetFile(self):
+        return self.fileChooser.getFilepath()
+
+
+class LayerVisualizerContainer(ttk.Frame):
+    def __init__(self, master=None, **kw):
+        ttk.Frame.__init__(self, master, **kw)
+
+        self.frameToExpand = tk.Frame(self)
+        self.frameToExpand.configure(height='200', width='200')
+        self.frameToExpand.pack(side='top')
+
+        self.LayerVisualizerRows = []
+        firstVisualizerRow = LayerVisualizerRow(master = self.frameToExpand, color = 5, index=0, changeCallback = self.changeCallback)
+        firstVisualizerRow.grid(column='0', row='0')
+        self.LayerVisualizerRows.append(firstVisualizerRow)
+
+    def changeCallback(self): #Carefull if modifying, look for recursion due to passing self.changeCallback to constructor of LayerVisualizerRow
+        if len(self.LayerVisualizerRows) == 0: #Function may get called way to early by initializers
+            return
+
+        if self.LayerVisualizerRows[-1] == None: #Needed to stop Recursion, this none step is important
+            return
+
+        lastFilename = self.LayerVisualizerRows[-1].GetFile()
+        twoBackFilename = None
+
+        if len(self.LayerVisualizerRows) > 1: #If the list is long enough, get the second back filename in list
+            twoBackFilename = self.LayerVisualizerRows[-2].GetFile()
+
+        if (not twoBackFilename == None) and (twoBackFilename.strip() == lastFilename.strip()) and (lastFilename.strip() == ''): #If the last two are empty, get rid of the last row
+            self.LayerVisualizerRows[-1].grid_forget()
+            del(self.LayerVisualizerRows[-1])
+
+        elif not lastFilename.strip() == '': #If the last row gets filled, create another row.
+            newIndex = len(self.LayerVisualizerRows)
+            self.LayerVisualizerRows.append(None)
+            nextVisualizerRow = LayerVisualizerRow(master = self.frameToExpand, color = self.getSuggestedColor(), index=newIndex, changeCallback = self.changeCallback)
+            nextVisualizerRow.grid(column='0', row=str(newIndex))
+            self.LayerVisualizerRows[-1] = nextVisualizerRow
+
+    def getSuggestedColor(self):
+        return 5
+
+#######################################
+# Main Application Class              #
+####################################### 
 
 class TabguiApp():
 	def __init__(self, master=None):
@@ -1025,7 +1216,6 @@ class TabguiApp():
 	def VisualizeButtonPress(self):
 		self.buttonOutputMakeGeometries['state'] = 'disabled'
 		fileToVisualize = self.pathchooserVisualize.entry.get()
-
 		memStream = MemoryStream()
 		t = threading.Thread(target=VisualizeThreadWorker, args=(fileToVisualize, memStream, 5))
 		t.setDaemon(True)
@@ -1342,15 +1532,9 @@ class TabguiApp():
 	def run(self):
 		self.mainwindow.mainloop()
 
-# print(sys.argv)
-# print(type(sys.argv))
-# # print('=======================================')
-# # print(get_args())
-# # print('=======================================')
-# # with redirect_argv(['--config-file ' + '/media/aaron/External/Spectroscopy_Images/_Official_GUI/Data/configs/MitoEM-R-BC.yaml', '--inference', '--checkpoint ' + '/media/aaron/External/Spectroscopy_Images/_Official_GUI/Data/models/TestModel/checkpoint_00010.pth.tar']):
-# # 	#print('--config-file ' + cfg + ' --inference' + ' --checkpoint ' + checkpoint)
-# # 	trainFromMain()
-# # exit()
+#######################################
+# Boilerplate TK Create & Run Window  #
+####################################### 
 
 if __name__ == '__main__':
 	import tkinter as tk
